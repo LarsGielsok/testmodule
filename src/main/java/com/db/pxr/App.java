b5e902e60af366c2b40944306aaaa9562f1b96d3
LarsGielsok <lars.gielsok@gmail.com>
2015-04-27 20:11:33 +0200

    initial push

---
+++ src/main/java/com/db/pxr/App.java
@@ -0,0 +1,16 @@
+package com.db.pxr;
+
+import java.lang.String;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public String echo(String message) {
+        return message;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
